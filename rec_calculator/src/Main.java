8fc4651d87e0b290cbb50ec01779cc43c1ec110d
ChanvisalYorn <89696454+YornChanvisal@users.noreply.github.com>
2024-03-04 02:30:44 +0700

    feat - let me tell you a secret, it is not working when multiply or divide with negative number

--- rec_calculator/src/Main.java
+++ rec_calculator/src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-
         String exp = "(1 + 2) * 10 - 6 / (2 + 1)";
-        String newExp = exp.replace(" ", "");
+        // (1 + 2) * 10 - 6 / (2 + 1) = 28.0
+        // (1 + 2 * 4 * 3) * 10 - 6 + 18 / (2 + (1  + 3) + 2 - (8 / 2 - 2)) = 247.0
 
-        double result = calculate(newExp);
+        double result = calculate(exp.replace(" ", ""));
 
         System.out.println("Result: " + result);
     }
@@ -20,53 +20,58 @@ public class Main {
             startIndex = exp.lastIndexOf("(");
             endIndex = exp.indexOf(")", startIndex);
             subExp = exp.substring(startIndex + 1, endIndex);
-            result = operate(subExp);
+            result = evaluate(subExp);
             exp = exp.replace(exp.substring(startIndex, endIndex + 1), String.valueOf(result));
             System.out.println(exp);
             return calculate(exp);
         } else {
-            //check if "*" and "/" exist then create a sub string to calculate it first and replace it
-            //all left is "+" and "-" we can do operation from left to right
-            String[] tokens = exp.split("[-+]");
+            return evaluate(exp);
+        }
+    }
+
+    public static double evaluate (String exp) {
+        double result = 0;
+        String[] tokens;
+
+        if (exp.contains("*")) {
+            tokens = exp.split("[-+/]");
             for (String token : tokens) {
-                if (token.contains("*") || token.contains("/")) {
-                    result = operate(token);
+                if (token.contains("*")) {
+                    String[] multiplyTokens = token.split("[*]");
+                    result = Double.parseDouble(multiplyTokens[0]);
+                    for (int i = 1; i < multiplyTokens.length; i++) {
+                        result *= Double.parseDouble(multiplyTokens[i]);
+                    }
                     exp = exp.replace(token, String.valueOf(result));
                     System.out.println(exp);
                 }
             }
+        }
 
+        if (exp.contains("/")) {
+            tokens = exp.split("[-+]");
+            for (String token : tokens) {
+                if (token.contains("/")) {
+                    String[] divideTokens = token.split("/");
+                    result = Double.parseDouble(divideTokens[0]);
+                    for (int i = 1; i < divideTokens.length; i++) {
+                        if (Double.parseDouble(divideTokens[i]) == 0.0) {
+                            throw new RuntimeException("Can't divide by 0");
+                        }
+                        result /= Double.parseDouble(divideTokens[i]);
+                    }
+                    exp = exp.replace(token, String.valueOf(result));
+                    System.out.println(exp);
+                }
+            }
+        }
+
+        if (exp.contains("+") || exp.contains("-")) {
             tokens = exp.split("(?=[+-])");
             result = 0;
             for (String token : tokens) {
                 result += Double.parseDouble(token);
             }
-
-            return result;
-        }
-    }
-
-    public static double operate (String exp) {
-        String[] operators = {"+", "-", "*", "/"};
-        String operator = "";
-        String[] operands = new String[2];
-
-        for(String op : operators) {
-            if(exp.contains(op)) {
-                operands = exp.split("[-+*/]");
-                operator = op;
-            }
-        }
-
-        double a = Double.parseDouble(operands[0]);
-        double b = Double.parseDouble(operands[1]);
-
-        double result = 0;
-        switch (operator) {
-            case "+" -> result = a + b;
-            case "-" -> result = a - b;
-            case "*" -> result = a * b;
-            case "/" -> result = a / b;
         }
 
         return result;
